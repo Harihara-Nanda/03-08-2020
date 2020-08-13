59a73ac23b1166db98d9207b162244d9524e351d
Harihara-Nanda <harryshnanda303@gmail.com>
2020-08-13 09:39:18 +0530

    Bootstrap files

---
+++ sample.js
@@ -0,0 +1,23 @@
+// // pre-defined functions
+// var d = new Date();
+// document.querySelector(".date").innerHTML=d;
+
+// // user defined functions
+
+// function example(){
+// 	document.write("HAI")
+// }
+
+// var x = function(a,b){
+// 	console.log(a*b)
+// }
+// x(4,5)
+
+// let y=(s,r)=> console.log(s*r);
+// y(10,9)
+var a=45;
+{
+	let a=80;
+	console.log(a);
+}
+console.log(a);
